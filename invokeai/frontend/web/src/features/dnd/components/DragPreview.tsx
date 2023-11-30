23004ce5538ee60932c00e4b8db2d54dd6cb5456
Vedant Dandge <92071471+vedant-3010@users.noreply.github.com>
2023-11-30 12:40:27 +0530

    Update DragPreview.tsx, improved code

--- invokeai/frontend/web/src/features/dnd/components/DragPreview.tsx
+++ invokeai/frontend/web/src/features/dnd/components/DragPreview.tsx
@@ -1,7 +1,6 @@
 import { Box, ChakraProps, Flex, Heading, Image, Text } from '@chakra-ui/react';
 import { memo } from 'react';
-import { TypesafeDraggableData } from 'features/dnd/types';
-import { useTranslation } from 'react-i18next';
+import { TypesafeDraggableData } from '../types';
 
 type OverlayDragImageProps = {
   dragData: TypesafeDraggableData | null;
@@ -27,76 +26,77 @@ const STYLES: ChakraProps['sx'] = {
 };
 
 const DragPreview = (props: OverlayDragImageProps) => {
-  const { t } = useTranslation();
-  if (!props.dragData) {
+  const { dragData } = props;
+
+  if (!dragData) {
     return null;
   }
 
-  if (props.dragData.payloadType === 'NODE_FIELD') {
-    const { field, fieldTemplate } = props.dragData.payload;
-    return (
-      <Box
-        sx={{
-          position: 'relative',
-          p: 2,
-          px: 3,
-          opacity: 0.7,
-          bg: 'base.300',
-          borderRadius: 'base',
-          boxShadow: 'dark-lg',
-          whiteSpace: 'nowrap',
-          fontSize: 'sm',
-        }}
-      >
-        <Text>{field.label || fieldTemplate.title}</Text>
-      </Box>
-    );
-  }
-
-  if (props.dragData.payloadType === 'IMAGE_DTO') {
-    const { thumbnail_url, width, height } = props.dragData.payload.imageDTO;
-    return (
-      <Box
-        sx={{
-          position: 'relative',
-          width: 'full',
-          height: 'full',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <Image
+  switch (dragData.payloadType) {
+    case 'NODE_FIELD': {
+      const { field, fieldTemplate } = dragData.payload;
+      return (
+        <Box
           sx={{
+            position: 'relative',
+            p: 2,
+            px: 3,
+            opacity: 0.7,
+            bg: 'base.300',
+            borderRadius: 'base',
+            boxShadow: 'dark-lg',
+            whiteSpace: 'nowrap',
+            fontSize: 'sm',
+          }}
+        >
+          <Text>{field.label || fieldTemplate.title}</Text>
+        </Box>
+      );
+    }
+    case 'IMAGE_DTO': {
+      const { thumbnail_url, width, height } = dragData.payload.imageDTO;
+      return (
+        <Box
+          sx={{
+            position: 'relative',
+            width: 'full',
+            height: 'full',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Image
+            sx={{
+              ...STYLES,
+            }}
+            objectFit="contain"
+            src={thumbnail_url}
+            width={width}
+            height={height}
+          />
+        </Box>
+      );
+    }
+    case 'IMAGE_DTOS': {
+      return (
+        <Flex
+          sx={{
+            position: 'relative',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDir: 'column',
             ...STYLES,
           }}
-          objectFit="contain"
-          src={thumbnail_url}
-          width={width}
-          height={height}
-        />
-      </Box>
-    );
+        >
+          <Heading>{dragData.payload.imageDTOs.length}</Heading>
+          <Heading size="sm">Images</Heading>
+        </Flex>
+      );
+    }
+    default:
+      return null;
   }
-
-  if (props.dragData.payloadType === 'IMAGE_DTOS') {
-    return (
-      <Flex
-        sx={{
-          position: 'relative',
-          alignItems: 'center',
-          justifyContent: 'center',
-          flexDir: 'column',
-          ...STYLES,
-        }}
-      >
-        <Heading>{props.dragData.payload.imageDTOs.length}</Heading>
-        <Heading size="sm">{t('parameters.images')}</Heading>
-      </Flex>
-    );
-  }
-
-  return null;
 };
 
 export default memo(DragPreview);
